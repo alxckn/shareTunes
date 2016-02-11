5732341f6f511ebdfc4087b5e2c636d6a4a3fff1
Mat <mat@gmail.com>
2016-02-11 23:14:16 +0100

    Add toolbar and sidenav

--- public/js/controllers/main.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by mathieu on 10/02/16.
- */
-
-var sharetunesApp = angular.module('sharetunesApp', []);
-
-sharetunesApp.controller('sharetunesCtrl', function($scope) {
-
-});
--- public/js/core.js
+++ public/js/core.js
@@ -1,5 +1,9 @@
 /**
  * Created by mathieu on 10/02/16.
  */
+(function() {
+    'use strict';
 
-angular.module('sharetunesApp', ['sharetunesCtrl']);
+angular.module( 'sharetunesApp', ['ngMaterial']);
+
+})();
