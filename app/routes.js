bffdc4131c5ca2b4462153f9ee18a15981be7c8b
Mat <mat@gmail.com>
2016-02-13 13:59:01 +0100

    Fix structure

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,11 @@
+var path = require('path');
+
 module.exports = function(app) {
-    app.get('/test', function(req, res) {
-        res.render('index');
+    app.get('/test', function (req, res) {
+        res.send('Hello World!');
     });
 
-    app.get('*', function(req, res) {
-        res.sendfile('./public/index.html');
+    app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname + '/index.html'));
     });
-
 };
--- public/js/core.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by mathieu on 10/02/16.
- */
-(function() {
-    'use strict';
-
-angular.module( 'sharetunesApp', ['ngMaterial']);
-
-})();
